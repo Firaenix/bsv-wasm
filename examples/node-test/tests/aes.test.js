5f1e2c217384e60848b25786bf7cab5c80e3e924
Firaenix <Nick.Carton@gmail.com>
2021-06-10 23:39:13 +1000

    Write AES tests

---
+++ examples/node-test/tests/aes.test.js
@@ -0,0 +1,17 @@
+import {AES} from '../../../pkg/bsv_wasm';
+import { assert, util } from 'chai';
+import { Aescbc } from "bsv";
+
+describe("AES Tests", function() {
+    it('AES Encrypted Message matches BSV.JS', () => {
+        const cipherKeyBuf = Buffer.alloc(256 / 8)
+        cipherKeyBuf.fill(0x10)
+        const ivBuf = Buffer.alloc(128 / 8)
+        ivBuf.fill(0)
+        const messageBuf = Buffer.from("Hello world");
+        const encBuf = Aescbc.encrypt(messageBuf, cipherKeyBuf, ivBuf, false);
+
+        let encrypted = AES.encrypt256(cipherKeyBuf, ivBuf, messageBuf);
+        assert.equal(Buffer.from(encrypted).toString('hex'), encBuf.toString('hex'))
+      });
+});
--- src/encryption/mod.rs
+++ src/encryption/mod.rs
@@ -1,7 +1,8 @@
 use aes::*;
-use block_modes::{BlockMode, Cbc, block_padding::NoPadding};
+use block_modes::{BlockMode, Cbc, block_padding::{NoPadding, Pkcs7}};
 use anyhow::*;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, throw_str, JsStatic};
+
 
 #[wasm_bindgen]
 pub struct AES {
@@ -9,28 +10,28 @@ pub struct AES {
 }
 
 impl AES {
-  pub fn encrypt_128_impl(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes128, NoPadding>::new_from_slices(key, iv).unwrap();
+  pub fn encrypt_128_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
+    let cipher = Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?;
 
-    Ok(cipher.encrypt(message, message.len())?.to_vec())
+    Ok(cipher.encrypt_vec(message))
   } 
 
-  pub fn decrypt_128_impl(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes128, NoPadding>::new_from_slices(key, iv).unwrap();
+  pub fn decrypt_128_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
+    let cipher = Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?;
 
-    Ok(cipher.decrypt(message)?.to_vec())
+    Ok(cipher.decrypt_vec(&message)?)
   }
 
-  pub fn encrypt_256_impl(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes256, NoPadding>::new_from_slices(key, iv).unwrap();
+  pub fn encrypt_256_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
+    let cipher = Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?;
 
-    Ok(cipher.encrypt(message, message.len())?.to_vec())
+    Ok(cipher.encrypt_vec(&message))
   }
 
-  pub fn decrypt_256_impl(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes256, NoPadding>::new_from_slices(key, iv).unwrap();
+  pub fn decrypt_256_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
+    let cipher = Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?;
 
-    Ok(cipher.decrypt(message)?.to_vec())
+    Ok(cipher.decrypt_vec(&message)?)
   }
 }
 
@@ -38,7 +39,7 @@ impl AES {
 #[wasm_bindgen]
 impl AES { 
   #[wasm_bindgen(js_name = encrypt128)]
-  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>, JsValue> {
+  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
     match AES::encrypt_128_impl(key, iv, message) {
       Ok(v) => Ok(v),
       Err(e) => throw_str(&e.to_string()),
@@ -46,7 +47,7 @@ impl AES {
   }
 
   #[wasm_bindgen(js_name = decrypt128)]
-  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>, JsValue> {
+  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
     match AES::decrypt_128_impl(key, iv, message) {
       Ok(v) => Ok(v),
       Err(e) => throw_str(&e.to_string()),
@@ -54,7 +55,7 @@ impl AES {
   }
 
   #[wasm_bindgen(js_name = encrypt256)]
-  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>, JsValue> {
+  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
     match AES::encrypt_256_impl(key, iv, message) {
       Ok(v) => Ok(v),
       Err(e) => throw_str(&e.to_string()),
@@ -62,7 +63,7 @@ impl AES {
   }
 
   #[wasm_bindgen(js_name = decrypt256)]
-  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>, JsValue> {
+  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
     match AES::decrypt_256_impl(key, iv, message) {
       Ok(v) => Ok(v),
       Err(e) => throw_str(&e.to_string()),
@@ -72,18 +73,18 @@ impl AES {
 
 #[cfg(not(target_arch = "wasm32"))]
 impl AES { 
-  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
+  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
     AES::encrypt_128_impl(key, iv, message)
   }
 
-  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
+  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
     AES::decrypt_128_impl(key, iv, message)
   }
-  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
+  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
     AES::encrypt_256_impl(key, iv, message)
   }
 
-  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &mut [u8]) -> Result<Vec<u8>> {
+  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
     AES::decrypt_256_impl(key, iv, message)
   }
 }
