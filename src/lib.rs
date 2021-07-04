89fe919b1b7ae7ca785aee8b614762bca6d75866
Firaenix <Nick.Carton@gmail.com>
2021-07-05 00:41:05 +1000

    remove console panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,6 @@ extern crate wee_alloc;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// #[cfg(target_arch = "wasm32")]
-// extern crate console_error_panic_hook;
-// // #[cfg(target_arch = "wasm32")]
-// console_error_panic_hook::set_once();
-
 pub mod keypair;
 
 pub use keypair::*;
