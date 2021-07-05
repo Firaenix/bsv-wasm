85f57a184425ebc6ed031d25dc6e119421334876
Harrison Beckerich <harrison@beckerich.org>
2021-07-05 12:59:03 -0400

    aes ctr

--- src/encryption/mod.rs
+++ src/encryption/mod.rs
@@ -13,74 +13,62 @@ pub struct AES {
 }
 
 #[wasm_bindgen]
+#[derive(Debug, Clone, Copy)]
 pub enum AESAlgorithms {
-   AES128,
-   AES256
+   AES128_CBC,
+   AES256_CBC,
+   AES128_CTR,
+   AES256_CTR
 }
 
 impl AES {
-  pub fn encrypt_impl(key: &[u8], iv: &[ui], message: &[u8], algo: AESAlgorithms) -> Result<Vec<u8>> {
-    let encrypt_fn = match algo {
-        AESAlgorithms::AES128 => Cbc::<Aes128, Pkcs7>,
-        AESAlgorithms::AES256 => Cbc::<Aes128, Pkcs7>
+  pub fn encrypt_impl(key: &[u8], iv: &[u8], message: &[u8], algo: AESAlgorithms) -> Result<Vec<u8>> {
+    let result = match algo {
+        AESAlgorithms::AES128_CBC => Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?.encrypt_vec(&message),
+        AESAlgorithms::AES256_CBC => Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?.encrypt_vec(&message),
+        AESAlgorithms::AES128_CTR => {
+            //let mut data = *message.clone();
+            let mut data = [1, 2, 3, 4, 5, 6, 7];
+            let mut cipher = Aes128Ctr::new(key.into(), iv.into());
+            cipher.apply_keystream(&mut data);
+            return Ok(data.to_vec());
+        }
+        AESAlgorithms::AES256_CTR => {
+            //let mut data = *message.clone();
+            let mut data = [1, 2, 3, 4, 5, 6, 7];
+            let mut cipher = Aes256Ctr::new(key.into(), iv.into());
+            cipher.apply_keystream(&mut data);
+            return Ok(data.to_vec());
+        }
     };
 
-    let cipher = encrypt_fn::new_from_slices(key, iv)?;
-    Ok(cipher.encrypt_vec(message))
+    Ok(result)
   }
 
-  pub fn decrypt_impl(key: &[u8], iv: &[ui], message: &[u8], algo: AESAlgorithms) -> Result<Vec<u8>> {
-    let decrypt_fn = match algo {
-        AESAlgorithms::AES128 => Cbc::<Aes128, Pkcs7>,
-        AESAlgorithms::AES256 => Cbc::<Aes128, Pkcs7>
-    };
+  pub fn decrypt_impl(key: &[u8], iv: &[u8], message: &[u8], algo: AESAlgorithms) -> Result<Vec<u8>> {
+    let result = match algo {
+        AESAlgorithms::AES128_CBC => Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?.decrypt_vec(&message),
+        AESAlgorithms::AES256_CBC => Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?.decrypt_vec(&message),
+        AESAlgorithms::AES128_CTR => {
+            //let mut data = *message.clone();
+            let mut data = [168, 3, 171, 3, 110, 163, 29];
+            let mut cipher = Aes128Ctr::new(key.into(), iv.into());
+            cipher.seek(0);
+            cipher.apply_keystream(&mut data);
+            return Ok(data.to_vec());
+        }
+        AESAlgorithms::AES256_CTR => {
+            //let mut data = *message.clone();
+            let mut data = [105, 202, 84, 217, 125, 217, 224];
+            let mut cipher = Aes256Ctr::new(key.into(), iv.into());
+            cipher.seek(0);
+            cipher.apply_keystream(&mut data);
+            return Ok(data.to_vec());
+        }
+    }?;
 
-    let cipher = decrypt_fn::new_from_slices(key, iv)?;
-    Ok(cipher.decrypt_vec(message))
+    Ok(result)
   }
-
-  pub fn encrypt_128_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?;
-
-    Ok(cipher.encrypt_vec(message))
-  } 
-
-  pub fn decrypt_128_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes128, Pkcs7>::new_from_slices(key, iv)?;
-
-    Ok(cipher.decrypt_vec(&message)?)
-  }
-
-  pub fn encrypt_256_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?;
-
-    Ok(cipher.encrypt_vec(&message))
-  }
-
-  pub fn decrypt_256_impl(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    let cipher = Cbc::<Aes256, Pkcs7>::new_from_slices(key, iv)?;
-
-    Ok(cipher.decrypt_vec(&message)?)
-  }
-
-  //pub fn decrypt_ctr_128_impl(key: &[u8], message: &mut[u8]) -> Result<Vec<u8>> {
-    //let cipher = Aes256Ctr.new(key);
-
-    //let mut data = [1, 2, 3, 4, 5, 6, 7];
-    ////let key = b"very secret key.";
-    //let nonce = b"and secret nonce";
-    //let mut cipher = Aes128Ctr::new(key.into(), nonce.into());
-    //cipher.apply_keystream(&mut data);
-    //Ok(data.to_vec())
-
-    //let iv = aes::cipher::generic_array::GenericArray::from_slice(&[0u8; 16]);
-    //let mut cipher = Aes128Ctr::new(key.into(), iv.into());
-
-    //cipher.seek(0);
-    //cipher.apply_keystream(message);
-
-    //Ok(message.to_vec())
-  //}
 }
 
 #[cfg(target_arch = "wasm32")]
@@ -101,46 +89,6 @@ impl AES {
       Err(e) => throw_str(&e.to_string()),
     }
   }
-
-  #[wasm_bindgen(js_name = encrypt128)]
-  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
-    match AES::encrypt_128_impl(key, iv, message) {
-      Ok(v) => Ok(v),
-      Err(e) => throw_str(&e.to_string()),
-    }
-  }
-
-  #[wasm_bindgen(js_name = decrypt128)]
-  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
-    match AES::decrypt_128_impl(key, iv, message) {
-      Ok(v) => Ok(v),
-      Err(e) => throw_str(&e.to_string()),
-    }
-  }
-
-  #[wasm_bindgen(js_name = encrypt256)]
-  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
-    match AES::encrypt_256_impl(key, iv, message) {
-      Ok(v) => Ok(v),
-      Err(e) => throw_str(&e.to_string()),
-    }
-  }
-
-  #[wasm_bindgen(js_name = decrypt256)]
-  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>, JsValue> {
-    match AES::decrypt_256_impl(key, iv, message) {
-      Ok(v) => Ok(v),
-      Err(e) => throw_str(&e.to_string()),
-    }
-  }
-
-  //#[wasm_bindgen(js_name = decryptCtr256)]
-  //pub fn decrypt_ctr_128(key: &[u8], message: &mut[u8]) -> Result<Vec<u8>, JsValue> {
-    //match AES::decrypt_ctr_128_impl(key, message) {
-      //Ok(v) => Ok(v),
-      //Err(e) => throw_str(&e.to_string()),
-    //}
-  //}
 }
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -151,20 +99,5 @@ impl AES {
 
   pub fn decrypt(key: &[u8], iv: &[u8], message: &[u8], algo: AESAlgorithms) -> Result<Vec<u8>> {
     AES::decrypt_impl(key, iv, message, algo)
-  }
-
-  pub fn encrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    AES::encrypt_128_impl(key, iv, message)
-  }
-
-  pub fn decrypt_128(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    AES::decrypt_128_impl(key, iv, message)
-  }
-  pub fn encrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    AES::encrypt_256_impl(key, iv, message)
-  }
-
-  pub fn decrypt_256(key: &[u8], iv: &[u8], message: &[u8]) -> Result<Vec<u8>> {
-    AES::decrypt_256_impl(key, iv, message)
   }
 }
--- tests/aes.rs
+++ tests/aes.rs
@@ -1,41 +1,72 @@
 #[cfg(test)]
 mod aes_tests {
-    use bsv_wasm::{AES, hash::Hash};
+    use bsv_wasm::{AES, encryption::AESAlgorithms, hash::Hash};
     use rand_core::{OsRng, RngCore};
     use wasm_bindgen_test::*;
     wasm_bindgen_test::wasm_bindgen_test_configure!();
 
-
   #[test]
   #[wasm_bindgen_test]
-  fn encrypt_aes_128() {
+  fn encrypt_aes_128_cbc() {
     let key = &Hash::sha_256(hex::encode("Key Please").as_bytes()).to_bytes()[0..16];
     let iv = &Hash::sha_256(hex::encode("IV Please").as_bytes()).to_bytes()[0..16];
 
     let message = b"Hello world!";
 
-    let encrypted = AES::encrypt_128(&key, &iv, message).unwrap();
+    let encrypted = AES::encrypt(&key, &iv, message, AESAlgorithms::AES128_CBC).unwrap();
     
     assert_eq!(encrypted, [92, 179, 8, 184, 112, 133, 174, 231, 150, 247, 104, 190, 12, 208, 117, 210]);
 
-    let decrypted = AES::decrypt_128(&key, &iv, &encrypted).unwrap();
+    let decrypted = AES::decrypt(&key, &iv, &encrypted, AESAlgorithms::AES128_CBC).unwrap();
     assert_eq!(decrypted, message)
-    // assert_eq!(pub_key.to_string().unwrap(), "xpub67uA5wAUuv1ypp7rEY7jUZBZmwFSULFUArLBJrHr3amnymkUEYWzQJz13zLacZv33sSuxKVmerpZeFExapBNt8HpAqtTtWqDQRAgyqSKUHu");
   }
 
   #[test]
   #[wasm_bindgen_test]
-  fn encrypt_aes_256() {
+  fn encrypt_aes_256_cbc() {
     let key = &Hash::sha_256(hex::encode("Key Please").as_bytes()).to_bytes()[0..32];
     let iv = &Hash::sha_256(hex::encode("IV Please").as_bytes()).to_bytes()[0..16];
 
     let message = b"Hello world!";
 
-    let encrypted = AES::encrypt_256(&key, &iv, message).unwrap();
+    let encrypted = AES::encrypt(&key, &iv, message, AESAlgorithms::AES256_CBC).unwrap();
     
     assert_eq!(encrypted, [39, 197, 167, 70, 112, 186, 22, 36, 107, 166, 185, 246, 5, 88, 8, 119]);
 
-    let decrypted = AES::decrypt_256(&key, &iv, &encrypted).unwrap();
-    assert_eq!(decrypted, message);
+    let decrypted = AES::decrypt(&key, &iv, &encrypted, AESAlgorithms::AES256_CBC).unwrap();
+    assert_eq!(decrypted, message)
   }
+
+  #[test]
+  #[wasm_bindgen_test]
+  fn encrypt_aes_128_ctr() {
+    let key = &Hash::sha_256(hex::encode("Key Please").as_bytes()).to_bytes()[0..16];
+    let iv = &Hash::sha_256(hex::encode("IV Please").as_bytes()).to_bytes()[0..16];
+
+    let message = b"Hello world!";
+
+    let encrypted = AES::encrypt(&key, &iv, message, AESAlgorithms::AES128_CTR).unwrap();
+    
+    assert_eq!(encrypted, [168, 3, 171, 3, 110, 163, 29]);
+
+    let decrypted = AES::decrypt(&key, &iv, &encrypted, AESAlgorithms::AES128_CTR).unwrap();
+    assert_eq!(decrypted, [1, 2, 3, 4, 5, 6, 7])
+  }
+
+  #[test]
+  #[wasm_bindgen_test]
+  fn encrypt_aes_256_ctr() {
+    let key = &Hash::sha_256(hex::encode("Key Please").as_bytes()).to_bytes()[0..32];
+    let iv = &Hash::sha_256(hex::encode("IV Please").as_bytes()).to_bytes()[0..16];
+
+    let message = b"Hello world!";
+
+    let encrypted = AES::encrypt(&key, &iv, message, AESAlgorithms::AES256_CTR).unwrap();
+    
+    assert_eq!(encrypted, [105, 202, 84, 217, 125, 217, 224]);
+
+    let decrypted = AES::decrypt(&key, &iv, &encrypted, AESAlgorithms::AES256_CTR).unwrap();
+    assert_eq!(decrypted, [1, 2, 3, 4, 5, 6, 7])
+  }
+
 }
