f354de3c45f2d14b47be3b17657ee7f5966bc8ab
Firaenix <Nick.Carton@gmail.com>
2021-07-23 00:25:44 +1000

    Ciphertexts not matching, needs further inspection

--- examples/node-test/tests/ecies.test.js
+++ examples/node-test/tests/ecies.test.js
@@ -12,29 +12,62 @@ describe("ECIES Tests", function() {
 
     let ciphertext = ECIES.encrypt(message, undefined, bob.getPublicKey());
     let ciphertextJs = Ecies.electrumEncrypt(message, bobJS.pubKey, null);
-
-    // assert.equal(Buffer.from(ciphertext).toString('hex'), ciphertextJs.toString('hex'));
+    
+    // Cant compare ciphertexts because different ephemeral keys
+    //  assert.equal(Buffer.from(ciphertext).toString('hex'), ciphertextJs.toString('hex'));
 
     let plaintext = ECIES.decrypt(ciphertext, bob, undefined);
     let plaintextJs = Ecies.electrumDecrypt(ciphertextJs, bobJS.privKey, null);
-    
+
     assert.equal(plaintextJs.toString('hex'), message.toString('hex'));
     assert.equal(Buffer.from(plaintext).toString('hex'), plaintextJs.toString('hex'));
 
     assert.equal(Buffer.from(plaintext).toString('hex'), message.toString('hex'));
   });
 
-  // it('ECIES BIE (Send to Self) Message matches BSV.JS', () => {
-  //   let message = Buffer.from("Hello, Bitcoin.");
+  it('ECIES BIE (Send to Self) Message matches BSV.JS', () => {
+    let message = Buffer.from("Hello, Bitcoin.");
 
+    // Sender + Recipient
+    let bob = PrivateKey.fromRandom();
+    let bobJS = KeyPair.fromPrivKey(PrivKey.fromWif(bob.toWIF()));
 
+    let ciphertext = ECIES.encrypt(message, bob, bob.getPublicKey());
+    let ciphertextJs = Ecies.electrumEncrypt(message, bobJS.pubKey, bobJS);
+    
+    assert.equal(Buffer.from(ciphertext).toString('hex'), ciphertextJs.toString('hex'), "Ciphertexts dont match");
 
-  // });
+    let plaintext = ECIES.decrypt(ciphertext, bob, undefined);
+    let plaintextJs = Ecies.electrumDecrypt(ciphertextJs, bobJS.privKey, null);
 
-  // it('ECIES BIE (Send to Other Party) Message matches BSV.JS', () => {
-  //   let message = Buffer.from("Hello, Bitcoin.");
+    assert.equal(plaintextJs.toString('hex'), message.toString('hex'));
+    assert.equal(Buffer.from(plaintext).toString('hex'), plaintextJs.toString('hex'));
 
+    assert.equal(Buffer.from(plaintext).toString('hex'), message.toString('hex'));
+  });
 
+  it('ECIES BIE (Send to Other Party) Message matches BSV.JS', () => {
+    let message = Buffer.from("Hello, Bitcoin.");
 
-  // });
+    // Sender
+    let alice = PrivateKey.fromRandom();
+    let aliceJS = KeyPair.fromPrivKey(PrivKey.fromWif(alice.toWIF()));
+
+    // Recipient
+    let bob = PrivateKey.fromRandom();
+    let bobJS = KeyPair.fromPrivKey(PrivKey.fromWif(bob.toWIF()));
+
+    let ciphertext = ECIES.encrypt(message, alice, bob.getPublicKey());
+    let ciphertextJs = Ecies.electrumEncrypt(message, bobJS.pubKey, aliceJS);
+
+    assert.equal(Buffer.from(ciphertext).toString('hex'), ciphertextJs.toString('hex'), "Ciphertexts dont match");
+
+    let plaintext = ECIES.decrypt(ciphertext, bob, alice.getPublicKey());
+    let plaintextJs = Ecies.electrumDecrypt(ciphertextJs, bobJS.privKey, aliceJS.pubKey);
+
+    assert.equal(plaintextJs.toString('hex'), message.toString('hex'));
+    assert.equal(Buffer.from(plaintext).toString('hex'), plaintextJs.toString('hex'));
+
+    assert.equal(Buffer.from(plaintext).toString('hex'), message.toString('hex'));
+  });
 });
--- src/ecdsa/ecdh.rs
+++ src/ecdsa/ecdh.rs
@@ -1,5 +1,5 @@
 use crate::{BSVErrors, PrivateKey, PublicKey};
-use elliptic_curve::ecdh;
+use elliptic_curve::ecdh::{self, diffie_hellman};
 use rand_core::OsRng;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{throw_str, JsValue};
@@ -7,31 +7,31 @@ use wasm_bindgen::{throw_str, JsValue};
 pub struct ECDH {}
 
 impl ECDH {
-    /**
-     * Derives the shared key between a recipients public key and an optional private key.
-     */
-    pub(crate) fn derive_shared_key_impl(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, BSVErrors> {
-        let internal_key = k256::PublicKey::from_sec1_bytes(&pub_key.to_bytes_impl()?)?;
-        let shared = elliptic_curve::ecdh::diffie_hellman(priv_key.secret_key.to_secret_scalar(), internal_key.as_affine());
-        let bytes = shared.as_bytes();
-        Ok(bytes.as_slice().to_vec())
-    }
+  /**
+   * Derives the shared key between a recipients public key and an optional private key.
+   */
+  pub(crate) fn derive_shared_key_impl(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, BSVErrors> {
+    let internal_key = k256::PublicKey::from_sec1_bytes(&pub_key.to_bytes_impl()?)?;
+    let shared = diffie_hellman(priv_key.secret_key.to_secret_scalar(), internal_key.as_affine());
+    let bytes = shared.as_bytes();
+    Ok(bytes.as_slice().to_vec())
+  }
 }
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 impl ECDH {
-    #[wasm_bindgen(js_name = deriveSharedKey)]
-    pub fn derive_shared_key(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, JsValue> {
-        match ECDH::derive_shared_key_impl(priv_key, pub_key) {
-            Ok(v) => Ok(v),
-            Err(e) => throw_str(&e.to_string()),
-        }
+  #[wasm_bindgen(js_name = deriveSharedKey)]
+  pub fn derive_shared_key(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, JsValue> {
+    match ECDH::derive_shared_key_impl(priv_key, pub_key) {
+      Ok(v) => Ok(v),
+      Err(e) => throw_str(&e.to_string()),
     }
+  }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 impl ECDH {
-    pub fn derive_shared_key(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, BSVErrors> {
-        ECDH::derive_shared_key_impl(priv_key, pub_key)
-    }
+  pub fn derive_shared_key(priv_key: &PrivateKey, pub_key: &PublicKey) -> Result<Vec<u8>, BSVErrors> {
+    ECDH::derive_shared_key_impl(priv_key, pub_key)
+  }
 }
