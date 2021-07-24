5944281403be380d60473713f327325acf8a69ad
Firaenix <Nick.Carton@gmail.com>
2021-07-24 11:39:26 +1000

    Make from_bytes static method

--- src/ecies/ecies_ciphertext.rs
+++ src/ecies/ecies_ciphertext.rs
@@ -18,6 +18,29 @@ impl ECIESCiphertext {
         let bytes = self.public_key_bytes.clone().ok_or_else(|| BSVErrors::ECIESError("No public key exists in this ciphertext".into()))?;
         PublicKey::from_bytes_impl(&bytes)
     }
+
+    pub fn from_bytes_impl(buffer: &[u8], has_pub_key: bool) -> Result<ECIESCiphertext, BSVErrors> {
+        let pub_key = match has_pub_key {
+            true => {
+                let pub_key_buf = &buffer[PUB_KEY_OFFSET as usize..PUB_KEY_END as usize];
+                PublicKey::from_bytes_impl(pub_key_buf)?;
+                Some(pub_key_buf.to_vec())
+            }
+            false => None,
+        };
+
+        let hmac = &buffer[buffer.len() - 32..buffer.len()];
+        let ciphertext = match has_pub_key {
+            true => &buffer[PUB_KEY_END as usize..buffer.len() - 32],
+            false => &buffer[4..buffer.len() - 32],
+        };
+
+        Ok(ECIESCiphertext {
+            public_key_bytes: pub_key,
+            hmac_bytes: hmac.into(),
+            ciphertext_bytes: ciphertext.into(),
+        })
+    }
 }
 
 #[wasm_bindgen]
@@ -44,26 +67,6 @@ impl ECIESCiphertext {
 
         buffer
     }
-
-    #[wasm_bindgen(js_name = fromBytes)]
-    pub fn from_bytes(&self, buffer: Vec<u8>, has_pub_key: bool) -> ECIESCiphertext {
-        let pub_key = match has_pub_key {
-            true => Some(buffer[PUB_KEY_OFFSET as usize..PUB_KEY_END as usize].to_vec()),
-            false => None,
-        };
-
-        let hmac = &buffer[buffer.len() - 32..buffer.len()];
-        let ciphertext = match has_pub_key {
-            true => &buffer[PUB_KEY_END as usize..buffer.len() - 32],
-            false => &buffer[4..buffer.len() - 32],
-        };
-
-        ECIESCiphertext {
-            public_key_bytes: pub_key,
-            hmac_bytes: hmac.into(),
-            ciphertext_bytes: ciphertext.into(),
-        }
-    }
 }
 
 #[cfg(target_arch = "wasm32")]
@@ -71,10 +74,12 @@ impl ECIESCiphertext {
 impl ECIESCiphertext {
     #[wasm_bindgen(js_name = extractPublicKey)]
     pub fn extract_public_key(&self) -> Result<PublicKey, JsValue> {
-        match self.extract_public_key_impl() {
-            Ok(v) => Ok(v),
-            Err(e) => throw_str(&e.to_string()),
-        }
+        self.extract_public_key_impl().map_err(|e| throw_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(js_name = fromBytes)]
+    pub fn from_bytes(buffer: &[u8], has_pub_key: bool) -> Result<ECIESCiphertext, JsValue> {
+        ECIESCiphertext::from_bytes_impl(buffer, has_pub_key).map_err(|e| throw_str(&e.to_string()))
     }
 }
 
@@ -82,5 +87,9 @@ impl ECIESCiphertext {
 impl ECIESCiphertext {
     pub fn extract_public_key(&self) -> Result<PublicKey, BSVErrors> {
         self.extract_public_key_impl()
+    }
+
+    pub fn from_bytes(buffer: &[u8], has_pub_key: bool) -> Result<ECIESCiphertext, BSVErrors> {
+        ECIESCiphertext::from_bytes_impl(buffer, has_pub_key)
     }
 }
