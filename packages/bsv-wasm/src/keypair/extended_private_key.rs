66ac001e00d779ca470eb3fb1a1be849aadb90c3
Nick Carton <nick.carton@protonmail.com>
2022-07-22 23:10:22 +0200

    XPrivs done

---
+++ packages/bsv-wasm/src/keypair/extended_private_key.rs
@@ -0,0 +1,68 @@
+use wasm_bindgen::prelude::*;
+use bsv::ExtendedPrivateKey as BSVExtendedPrivateKey;
+
+use super::{private_key::PrivateKey, public_key::PublicKey};
+
+#[wasm_bindgen]
+pub struct ExtendedPrivateKey(pub(crate) BSVExtendedPrivateKey);
+
+
+impl ExtendedPrivateKey {
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getPrivateKey))]
+    pub fn get_private_key(&self) -> PrivateKey {
+        PrivateKey(self.0.get_private_key())
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getPublicKey))]
+    pub fn get_public_key(&self) -> PublicKey {
+        PublicKey(self.0.get_public_key())
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getChainCode))]
+    pub fn get_chain_code(&self) -> Vec<u8> {
+        self.0.get_chain_code()
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getDepth))]
+    pub fn get_depth(&self) -> u8 {
+        self.0.get_depth()
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getParentFingerprint))]
+    pub fn get_parent_fingerprint(&self) -> Vec<u8> {
+        self.0.get_parent_fingerprint()
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getIndex))]
+    pub fn get_index(&self) -> u32 {
+        self.0.get_index()
+    }
+
+    pub fn derive(&self, index: u32) -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(self.0.derive(index)?))
+    }
+
+    pub fn derive_from_path(&self, path: &str) -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(self.0.derive_from_path(path)?))
+    }
+
+    pub fn from_seed(seed: &[u8]) -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(BSVExtendedPrivateKey::from_seed_impl(seed)?))
+    }
+
+    pub fn from_random() -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(BSVExtendedPrivateKey::from_random()?))
+    }
+
+    pub fn from_string(xprv_string: &str) -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(BSVExtendedPrivateKey::from_string(xprv_string)?))
+    }
+
+    pub fn to_string(&self) -> Result<String, wasm_bindgen::JsError> {
+        Ok(self.0.to_string()?)
+    }
+
+    pub fn from_mnemonic(mnemonic: &[u8], passphrase: Option<Vec<u8>>) -> Result<ExtendedPrivateKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPrivateKey(BSVExtendedPrivateKey::from_mnemonic(mnemonic, passphrase)?))
+    }
+}
---
+++ packages/bsv-wasm/src/keypair/extended_public_key.rs
@@ -0,0 +1,61 @@
+use bsv::ExtendedPublicKey as BSVExtendedPublicKey;
+use wasm_bindgen::prelude::*;
+
+use crate::keypair::{public_key::PublicKey, extended_private_key::ExtendedPrivateKey};
+
+#[wasm_bindgen]
+pub struct ExtendedPublicKey(pub(crate) BSVExtendedPublicKey);
+
+#[wasm_bindgen]
+impl ExtendedPublicKey {
+    pub fn get_public_key(&self) -> PublicKey {
+        PublicKey(self.0.get_public_key())
+    }
+
+    pub fn from_xpriv(xpriv: &ExtendedPrivateKey) -> Self {
+       ExtendedPublicKey(BSVExtendedPublicKey::from_xpriv(&xpriv.0)) 
+    }
+
+    pub fn get_chain_code(&self) -> Vec<u8> {
+        self.0.get_chain_code()
+    }
+
+    // #[cfg_attr(all(feature = "wasm-bindgen-keypair"), wasm_bindgen(js_name = getDepth))]
+    pub fn get_depth(&self) -> u8 {
+        self.0.get_depth()
+    }
+
+    pub fn get_parent_fingerprint(&self) -> Vec<u8> {
+        self.0.get_parent_fingerprint()
+    }
+
+    pub fn get_index(&self) -> u32 {
+        self.0.get_index()
+    }
+
+    pub fn derive(&self, index: u32) -> Result<ExtendedPublicKey, wasm_bindgen::JsError> {
+        Ok(self.derive(index)?)
+    }
+
+    pub fn derive_from_path(&self, path: &str) -> Result<ExtendedPublicKey, wasm_bindgen::JsError> {
+        Ok(self.derive_from_path(path)?)
+    }
+
+    pub fn from_seed(seed: &[u8]) -> Result<ExtendedPublicKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPublicKey(BSVExtendedPublicKey::from_seed(seed)?))
+    }
+
+    pub fn from_random() -> Result<ExtendedPublicKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPublicKey(BSVExtendedPublicKey::from_random()?))
+    }
+
+    pub fn from_string(xpub_string: &str) -> Result<ExtendedPublicKey, wasm_bindgen::JsError> {
+        Ok(ExtendedPublicKey(BSVExtendedPublicKey::from_string(xpub_string)?))
+    }
+
+    pub fn to_string(&self) -> Result<String, wasm_bindgen::JsError> {
+        Ok(self.0.to_string()?)
+    }
+}
+
+
