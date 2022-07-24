405deb67ed94b5fe01a245b5dc3b19841b033c4b
Nick Carton <nick.carton@protonmail.com>
2022-07-24 22:36:13 +0200

    Added TxOut

--- packages/bsv-wasm/src/transaction/txin.rs
+++ packages/bsv-wasm/src/transaction/txin.rs
@@ -37,12 +37,10 @@ impl TxIn {
        Script(self.0.get_unlocking_script())
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getUnlockingScriptHex))]
     pub fn get_unlocking_script_hex(&self) -> String {
        self.0.get_unlocking_script_hex() 
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getSequence))]
     pub fn get_sequence(&self) -> u32 {
         self.0.get_sequence()
     }
@@ -51,57 +49,46 @@ impl TxIn {
         self.0.get_sequence_as_bytes()
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getOutpointBytes))]
     pub fn get_outpoint_bytes(&self, little_endian: Option<bool>) -> Vec<u8> {
         self.0.get_outpoint_bytes(little_endian)
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getOutpointHex))]
     pub fn get_outpoint_hex(&self, little_endian: Option<bool>) -> String {
         self.0.get_outpoint_hex(little_endian)
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setUnlockingScript))]
     pub fn set_unlocking_script(&mut self, script: &Script) {
         self.0.set_unlocking_script(&script.0)
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setPrevTxId))]
     pub fn set_prev_tx_id(&mut self, txid: &[u8]) {
         self.0.set_prev_tx_id(txid)
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setVOut))]
     pub fn set_vout(&mut self, vout: u32) {
         self.0.set_vout(vout);
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setSequence))]
     pub fn set_sequence(&mut self, sequence: u32) {
         self.0.set_sequence(sequence);
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setSatoshis))]
     pub fn set_satoshis(&mut self, satoshis: u64) {
         self.0.set_satoshis(satoshis);
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getSatoshis))]
     pub fn get_satoshis(&self) -> Option<u64> {
         self.0.get_satoshis()
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = setLockingScript))]
     pub fn set_locking_script(&mut self, locking_script: &Script) {
         self.0.set_locking_script(&locking_script.0)
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getLockingScript))]
     pub fn get_locking_script(&self) -> Option<Script> {
         self.0.get_locking_script().map(|x| Script(x))
     }
 
-    // #[cfg_attr(all(feature = "wasm-bindgen-transaction"), wasm_bindgen(js_name = getLockingScriptBytes))]
     pub fn get_locking_script_bytes(&self) -> Option<Vec<u8>> {
         self.0.get_locking_script_bytes()
     }
--- packages/bsv-wasm/src/transaction/txout.rs
+++ packages/bsv-wasm/src/transaction/txout.rs
@@ -1,5 +1,57 @@
 use bsv::TxOut as BSVTxOut;
 use wasm_bindgen::prelude::*;
 
+use crate::script::Script;
+
 #[wasm_bindgen]
 pub struct TxOut(pub(crate) BSVTxOut);
+
+#[wasm_bindgen]
+impl TxOut {
+    #[wasm_bindgen(constructor)]
+    pub fn new(value: u64, script_pub_key: &Script) -> TxOut {
+       TxOut(BSVTxOut::new(value, &script_pub_key.0)) 
+    }
+
+    pub fn get_satoshis(&self) -> u64 {
+        self.0.get_satoshis()
+    }
+
+    pub fn get_satoshis_as_bytes(&self) -> Vec<u8> {
+        self.0.get_satoshis_as_bytes()
+    }
+
+    pub fn get_script_pub_key_size(&self) -> usize {
+        self.0.get_script_pub_key_size()
+    }
+
+    pub fn get_script_pub_key(&self) -> Script {
+        Script(self.0.get_script_pub_key())
+    }
+
+    pub fn get_script_pub_key_hex(&self) -> String {
+        self.0.get_script_pub_key_hex()
+    }
+
+    pub fn from_hex(hex_str: &str) -> Result<TxOut, wasm_bindgen::JsError> {
+        Ok(TxOut(BSVTxOut::from_hex(hex_str)?))
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, wasm_bindgen::JsError> {
+        Ok(self.0.to_bytes()?)
+    }
+
+    pub fn to_hex(&self) -> Result<String, wasm_bindgen::JsError> {
+        Ok(self.0.to_hex()?)
+    }
+
+    pub fn to_json(&self) -> Result<JsValue, JsError> {
+        Ok(serde_wasm_bindgen::to_value(&self.0)?)
+    }
+
+    pub fn to_json_string(&self) -> Result<String, wasm_bindgen::JsError> {
+        Ok(self.to_json_string()?)
+    }
+}
+
+
